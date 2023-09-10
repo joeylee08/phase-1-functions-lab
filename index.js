644f04a88806e78e766af8008eb4117a01c98b03
Joseph <josephylee72@gmail.com>
2023-09-09 21:24:13 -0700

    completed functions lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(location) {
+  return Math.abs(location - 42);
+}
+
+function distanceFromHqInFeet(location) {
+  return distanceFromHqInBlocks(location) * 264;
+}
+
+function distanceTravelledInFeet(start, end) {
+  const sorted = [...arguments].sort((a, b) => a - b);
+  return (sorted[1] - sorted[0]) * 264;
+}
+
+function calculatesFarePrice(start, end) {
+  const distance = distanceTravelledInFeet(start, end);
+  if (distance <= 400) return 0;
+  else if (distance > 400 && distance <= 2000) return (distance - 400) * 0.02;
+  else if (distance > 2000 && distance <= 2500) return 25;
+  else return "cannot travel that far";
+}
